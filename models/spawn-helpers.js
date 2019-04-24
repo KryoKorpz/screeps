ead246943d1c5c44bf971efc9983830a4732da16
unknown <jesse.reichel@live.com>
2019-04-24 11:28:16 -0700

    refactored partBuilders for better scalability

--- models/spawn-helpers.js
+++ models/spawn-helpers.js
@@ -1,23 +1,47 @@
 const nonAttackPartBuilder = (availEnergyCapacity) => {
-    const workPartEnergy = Math.floor(availEnergyCapacity/2/100);
-    const remainingPartEnergy = Math.ceil(availEnergyCapacity/4/50)
+    const workPartEnergy = Math.floor(availEnergyCapacity/2);
     const partList = [];
     let partCost = 0
-    for (let i = 0; i < workPartEnergy; i++) {
+    for (let i = 0; i < workPartEnergy/100; i++) {
     partList.push(WORK);
     partCost += 100;
     }
+    let workPartListCount = partList.length
     
-    for (let i = 1; i < remainingPartEnergy; i++) {
-    partList.push(CARRY);
-    partCost += 50;
+    while(workPartListCount >= 0) {
+            partList.push(MOVE);
+            partCost += 50;
+            workPartListCount -= 1;
+    }
+    while(partCost < availEnergyCapacity) {
+            partList.push(CARRY);
+            partCost += 50;
     }
     
-    for (let i = 0; i < remainingPartEnergy; i++) {
-    partList.push(MOVE);
-    partCost += 50;
+    return {
+        partList, 
+        partCost
     }
-    
+}
+
+const harvestPartBuilder = (availEnergyCapacity) => {
+    const workPartEnergy = Math.floor(availEnergyCapacity*.75/100);
+    const remainingPartEnergy = Math.ceil(availEnergyCapacity*.5/50)
+    const partList = [];
+    let partCost = 0
+    for (let i = 1; i < workPartEnergy; i++) {
+        partList.push(WORK);
+        partCost += 100;
+        }
+
+    while(partCost < availEnergyCapacity -50) {
+        partList.push(MOVE)
+        partCost += 50
+    }
+    partList.push(CARRY)
+    partCost += 50
+
+
     return {
         partList, 
         partCost
@@ -51,6 +75,25 @@ const creepNonAttackSpawn = (creepRole, creepRoles, spawnCount, gameRoom, spawnL
                 }
             }
 }
+
+const creepHarvesterSpawn = (creepRole, creepRoles, spawnCount, gameRoom, spawnLoc) => {
+        const availEnergy = Game.rooms[gameRoom].energyAvailable;
+        const availEnergyCapacity = Game.rooms[gameRoom].energyCapacityAvailable;
+        const parts = harvestPartBuilder(availEnergyCapacity);
+        const partListLength = parts.partList.length
+        const partCost = parts.partCost
+        
+        
+        if (creepRoles.length < spawnCount && availEnergy === availEnergyCapacity) {
+            var newName = creepRole + Game.time;
+            console.log('Spawning new ' + creepRole + " " + newName + " Total Parts: " + partListLength + " Part Cost: " + partCost + "/" + availEnergy);
+            const result = Game.spawns[spawnLoc].spawnCreep(parts.partList, newName,
+                    {memory: {role: creepRole}})
+                if (result != OK) {
+                    console.log("error code: " + result);
+                }
+            }
+}
     
 const creepRepairerSpawn = (creepRole, creepRoles, spawnCount, gameRoom, altEnergy, spawnLoc) => {
         const availEnergy = Game.rooms[gameRoom].energyAvailable;
@@ -65,18 +108,6 @@ const creepRepairerSpawn = (creepRole, creepRoles, spawnCount, gameRoom, altEner
                     console.log("error code: " + result);
                 }
         }
-}
-
-const creepHarvesterSpawn = (creepRole, creepRoles, spawnCount, gameRoom, spawnLoc) => {
-        const availEnergy = Game.rooms[gameRoom].energyAvailable;
-        const availEnergyCapacity = Game.rooms[gameRoom].energyCapacityAvailable;
-
-        if (creepRoles.length < spawnCount && availEnergy === availEnergyCapacity) {
-            var newName = creepRole + Game.time;
-            console.log('Spawning new ' + creepRole + " " + newName );
-            Game.spawns[spawnLoc].spawnCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE], newName,
-                {memory: {role: creepRole}})
-            }
 }
 
 const creepUpgraderSpawn = (creepRole, creepRoles, spawnCount, gameRoom, spawnLoc) => {
--- models/test.js
+++ models/test.js
@@ -1,21 +1,22 @@
 const nonAttackPartBuilder = (availEnergyCapacity) => {
-    const workPartEnergy = Math.floor(availEnergyCapacity/2/100);
-    const remainingPartEnergy = Math.ceil(availEnergyCapacity/4/50)
+    const workPartEnergy = Math.floor(availEnergyCapacity/2);
+    console.log(workPartEnergy)
     const partList = [];
     let partCost = 0
-    for (let i = 0; i < workPartEnergy; i++) {
-    partList.push("WORK");
+    for (let i = 0; i < workPartEnergy/100; i++) {
+    partList.push('WORK');
     partCost += 100;
     }
+    let workPartListCount = partList.length
     
-    for (let i = 1; i < remainingPartEnergy; i++) {
-    partList.push("CARRY");
-    partCost += 50;
+    while(workPartListCount >= 0) {
+            partList.push('MOVE');
+            partCost += 50;
+            workPartListCount -= 1;
     }
-    
-    for (let i = 0; i < remainingPartEnergy; i++) {
-    partList.push("MOVE");
-    partCost += 50;
+    while(partCost < availEnergyCapacity) {
+            partList.push('CARRY');
+            partCost += 50;
     }
     
     return {
@@ -24,5 +25,16 @@ const nonAttackPartBuilder = (availEnergyCapacity) => {
     }
 }
 
-const parts = nonAttackPartBuilder(650)
-console.log(parts.partList)
+const result1 = nonAttackPartBuilder(700)
+const result2 = nonAttackPartBuilder(750)
+const result3 = nonAttackPartBuilder(800)
+const result4 = nonAttackPartBuilder(900)
+
+console.log(result1.partList)
+console.log(result1.partCost)
+console.log(result2.partList)
+console.log(result2.partCost)
+console.log(result3.partList)
+console.log(result3.partCost)
+console.log(result4.partList)
+console.log(result4.partCost)
